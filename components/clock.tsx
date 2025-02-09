78e029f5822cc3c1cbac6bbebfa529e8b6dd76e2
Andrea Zago <info@andreazago.it>
2025-02-09 13:51:07 +0100

    feature: add suppression for clock

--- components/clock.tsx
+++ components/clock.tsx
@@ -2,20 +2,29 @@
 import React, { useEffect, useState } from "react";
 
 const Clock = () => {
-  const [time, setTime] = useState(new Date()); // Stato per tenere traccia dell'ora
+  const [time, setTime] = useState<Date | null>(null); // Initialize time as null
+  const [isClient, setIsClient] = useState(false); // State to track if component is mounted on client
 
   useEffect(() => {
+    setIsClient(true); // Component is now mounted on the client
     const interval = setInterval(() => {
-      setTime(new Date()); // Ogni secondo aggiorniamo l'ora
+      setTime(new Date()); // Update time every second
     }, 1000);
 
-    // Pulisce l'intervallo quando il componente viene smontato
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); // Clean up interval on component unmount
   }, []);
 
-  const formattedTime = time.toLocaleTimeString();
+  if (!isClient) {
+    return null; // Render nothing on the server
+  }
 
-  return <p className="text-sm md:text-xl">{formattedTime}</p>;
+  const formattedTime = time ? time.toLocaleTimeString() : ""; // Format time if available
+
+  return (
+    <p className="text-sm md:text-xl" suppressHydrationWarning>
+      {formattedTime}
+    </p>
+  );
 };
 
 export default Clock;
